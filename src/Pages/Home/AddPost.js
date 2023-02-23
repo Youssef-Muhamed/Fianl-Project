2670be5bf36d6a0bf0a652f1fd35414a98b26e89
Michael Rezq <michaelrezq9@gmail.com>
2023-02-24 01:02:10 +0200

    update

--- src/Pages/Home/AddPost.js
+++ src/Pages/Home/AddPost.js
@@ -4,6 +4,45 @@ const AddPost = () => {
   return (
     <div>
       {/* create post */}
+      <div>
+        {/* Button trigger modal */}
+        <button type="button" className="btn btn-primary">
+          Launch demo modal
+        </button>
+        {/* Modal */}
+        <div className="modal fade" id="exampleModal" tabIndex={-1}>
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h1 className="modal-title fs-5" id="exampleModalLabel">
+                  Modal title
+                </h1>
+                <button
+                  type="button"
+                  className="btn-close"
+                  data-bs-dismiss="modal"
+                  aria-label="Close"
+                />
+              </div>
+              <div className="modal-body">...</div>
+              <div className="modal-footer">
+                <button
+                  type="button"
+                  className="btn btn-secondary"
+                  data-bs-dismiss="modal"
+                >
+                  Close
+                </button>
+                <button type="button" className="btn btn-primary">
+                  Save changes
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* kk */}
       <div className="bg-white rounded-2 p-3 mb-3 border">
         <div className="inputPost d-flex justify-content-evenly align-items-center">
           <div className="text-center">
@@ -13,6 +52,8 @@ const AddPost = () => {
             type="text"
             placeholder="start a post"
             className="rounded-pill px-3 py-2 w-75 border"
+            data-bs-toggle="modal"
+            data-bs-target="#exampleModal"
           />
         </div>
         <div className="buttons d-flex justify-content-between mt-2">
