7d41c074770f5231e2724ebe5298e881b2b238ae
youssef <eng.youssef2023@gmail.com>
2023-02-25 20:33:11 +0200

    solve update

--- src/Pages/Jobs/AllJobs/ListJobs.js
+++ src/Pages/Jobs/AllJobs/ListJobs.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 import './Style.css'
 function ListJobs() {
   return (
@@ -14,21 +15,10 @@ function ListJobs() {
           <li><a href="#">Intern</a></li>
           <li><a href="#">part Time</a></li>
         </ul>
+
+
         <div className="single-post d-flex flex-row">
-          <div className="thumb">
-            <img src="img/post.png" alt />
-            <ul className="tags">
-              <li>
-                <a href="#">Art</a>
-              </li>
-              <li>
-                <a href="#">Media</a>
-              </li>
-              <li>
-                <a href="#">Design</a>					
-              </li>
-            </ul>
-          </div>
+          
           <div className="details">
             <div className="title d-flex flex-row justify-content-between">
               <div className="titles">
@@ -36,7 +26,7 @@ function ListJobs() {
                 <h6>Premium Labels Limited</h6>					
               </div>
               <ul className="btns">
-                <li><a href="#"><span className="lnr lnr-heart" /></a></li>
+                <li><a href="#"><span className="fa-solid fa-heart" /></a></li>
                 <li><a href="#">Apply</a></li>
               </ul>
             </div>
@@ -44,25 +34,14 @@ function ListJobs() {
               Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temporinc ididunt ut dolore magna aliqua.
             </p>
             <h5>Job Nature: Full time</h5>
-            <p className="address"><span className="lnr lnr-map" /> 56/8, Panthapath Dhanmondi Dhaka</p>
-            <p className="address"><span className="lnr lnr-database" /> 15k - 25k</p>
+            <p className="address"><span className="fa-solid fa-map" /> 56/8, Panthapath Dhanmondi Dhaka</p>
+            <p className="address"><span className="fa-solid fa-database" /> 15k - 25k</p>
           </div>
         </div>
+
+
         <div className="single-post d-flex flex-row">
-          <div className="thumb">
-            <img src="img/post.png" alt />
-            <ul className="tags">
-              <li>
-                <a href="#">Art</a>
-              </li>
-              <li>
-                <a href="#">Media</a>
-              </li>
-              <li>
-                <a href="#">Design</a>					
-              </li>
-            </ul>
-          </div>
+          
           <div className="details">
             <div className="title d-flex flex-row justify-content-between">
               <div className="titles">
@@ -70,7 +49,7 @@ function ListJobs() {
                 <h6>Premium Labels Limited</h6>					
               </div>
               <ul className="btns">
-                <li><a href="#"><span className="lnr lnr-heart" /></a></li>
+                <li><a href="#"><span className="fa-solid fa-heart" /></a></li>
                 <li><a href="#">Apply</a></li>
               </ul>
             </div>
@@ -78,25 +57,14 @@ function ListJobs() {
               Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temporinc ididunt ut dolore magna aliqua.
             </p>
             <h5>Job Nature: Full time</h5>
-            <p className="address"><span className="lnr lnr-map" /> 56/8, Panthapath Dhanmondi Dhaka</p>
-            <p className="address"><span className="lnr lnr-database" /> 15k - 25k</p>
+            <p className="address"><span className="fa-solid fa-map" /> 56/8, Panthapath Dhanmondi Dhaka</p>
+            <p className="address"><span className="fa-solid fa-database" /> 15k - 25k</p>
           </div>
         </div>
+
+
         <div className="single-post d-flex flex-row">
-          <div className="thumb">
-            <img src="img/post.png" alt />
-            <ul className="tags">
-              <li>
-                <a href="#">Art</a>
-              </li>
-              <li>
-                <a href="#">Media</a>
-              </li>
-              <li>
-                <a href="#">Design</a>					
-              </li>
-            </ul>
-          </div>
+          
           <div className="details">
             <div className="title d-flex flex-row justify-content-between">
               <div className="titles">
@@ -104,7 +72,7 @@ function ListJobs() {
                 <h6>Premium Labels Limited</h6>					
               </div>
               <ul className="btns">
-                <li><a href="#"><span className="lnr lnr-heart" /></a></li>
+                <li><a href="#"><span className="fa-solid fa-heart" /></a></li>
                 <li><a href="#">Apply</a></li>
               </ul>
             </div>
@@ -112,59 +80,14 @@ function ListJobs() {
               Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temporinc ididunt ut dolore magna aliqua.
             </p>
             <h5>Job Nature: Full time</h5>
-            <p className="address"><span className="lnr lnr-map" /> 56/8, Panthapath Dhanmondi Dhaka</p>
-            <p className="address"><span className="lnr lnr-database" /> 15k - 25k</p>
-          </div>
-        </div>		
-        <div className="single-post d-flex flex-row">
-          <div className="thumb">
-            <img src="img/post.png" alt />
-            <ul className="tags">
-              <li>
-                <a href="#">Art</a>
-              </li>
-              <li>
-                <a href="#">Media</a>
-              </li>
-              <li>
-                <a href="#">Design</a>					
-              </li>
-            </ul>
-          </div>
-          <div className="details">
-            <div className="title d-flex flex-row justify-content-between">
-              <div className="titles">
-                <a href="single.html"><h4>Creative Art Designer</h4></a>
-                <h6>Premium Labels Limited</h6>					
-              </div>
-              <ul className="btns">
-                <li><a href="#"><span className="lnr lnr-heart" /></a></li>
-                <li><a href="#">Apply</a></li>
-              </ul>
-            </div>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temporinc ididunt ut dolore magna aliqua.
-            </p>
-            <h5>Job Nature: Full time</h5>
-            <p className="address"><span className="lnr lnr-map" /> 56/8, Panthapath Dhanmondi Dhaka</p>
-            <p className="address"><span className="lnr lnr-database" /> 15k - 25k</p>
+            <p className="address"><span className="fa-solid fa-map" /> 56/8, Panthapath Dhanmondi Dhaka</p>
+            <p className="address"><span className="fa-solid fa-database" /> 15k - 25k</p>
           </div>
         </div>
+
+
         <div className="single-post d-flex flex-row">
-          <div className="thumb">
-            <img src="img/post.png" alt />
-            <ul className="tags">
-              <li>
-                <a href="#">Art</a>
-              </li>
-              <li>
-                <a href="#">Media</a>
-              </li>
-              <li>
-                <a href="#">Design</a>					
-              </li>
-            </ul>
-          </div>
+          
           <div className="details">
             <div className="title d-flex flex-row justify-content-between">
               <div className="titles">
@@ -172,7 +95,7 @@ function ListJobs() {
                 <h6>Premium Labels Limited</h6>					
               </div>
               <ul className="btns">
-                <li><a href="#"><span className="lnr lnr-heart" /></a></li>
+                <li><a href="#"><span className="fa-solid fa-heart" /></a></li>
                 <li><a href="#">Apply</a></li>
               </ul>
             </div>
@@ -180,25 +103,14 @@ function ListJobs() {
               Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temporinc ididunt ut dolore magna aliqua.
             </p>
             <h5>Job Nature: Full time</h5>
-            <p className="address"><span className="lnr lnr-map" /> 56/8, Panthapath Dhanmondi Dhaka</p>
-            <p className="address"><span className="lnr lnr-database" /> 15k - 25k</p>
+            <p className="address"><span className="fa-solid fa-map" /> 56/8, Panthapath Dhanmondi Dhaka</p>
+            <p className="address"><span className="fa-solid fa-database" /> 15k - 25k</p>
           </div>
         </div>
+
+
         <div className="single-post d-flex flex-row">
-          <div className="thumb">
-            <img src="img/post.png" alt />
-            <ul className="tags">
-              <li>
-                <a href="#">Art</a>
-              </li>
-              <li>
-                <a href="#">Media</a>
-              </li>
-              <li>
-                <a href="#">Design</a>					
-              </li>
-            </ul>
-          </div>
+          
           <div className="details">
             <div className="title d-flex flex-row justify-content-between">
               <div className="titles">
@@ -206,7 +118,7 @@ function ListJobs() {
                 <h6>Premium Labels Limited</h6>					
               </div>
               <ul className="btns">
-                <li><a href="#"><span className="lnr lnr-heart" /></a></li>
+                <li><a href="#"><span className="fa-solid fa-heart" /></a></li>
                 <li><a href="#">Apply</a></li>
               </ul>
             </div>
@@ -214,25 +126,14 @@ function ListJobs() {
               Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temporinc ididunt ut dolore magna aliqua.
             </p>
             <h5>Job Nature: Full time</h5>
-            <p className="address"><span className="lnr lnr-map" /> 56/8, Panthapath Dhanmondi Dhaka</p>
-            <p className="address"><span className="lnr lnr-database" /> 15k - 25k</p>
+            <p className="address"><span className="fa-solid fa-map" /> 56/8, Panthapath Dhanmondi Dhaka</p>
+            <p className="address"><span className="fa-solid fa-database" /> 15k - 25k</p>
           </div>
-        </div>															
+        </div>
+
+
         <div className="single-post d-flex flex-row">
-          <div className="thumb">
-            <img src="img/post.png" alt />
-            <ul className="tags">
-              <li>
-                <a href="#">Art</a>
-              </li>
-              <li>
-                <a href="#">Media</a>
-              </li>
-              <li>
-                <a href="#">Design</a>					
-              </li>
-            </ul>
-          </div>
+          
           <div className="details">
             <div className="title d-flex flex-row justify-content-between">
               <div className="titles">
@@ -240,7 +141,8 @@ function ListJobs() {
                 <h6>Premium Labels Limited</h6>					
               </div>
               <ul className="btns">
-                <li><a href="#"><span className="lnr lnr-heart" /></a></li>
+              <i class="fa-solid fa-heart"></i>
+                <li><a href="#"><span className="fa-solid fa-heart" /></a></li>
                 <li><a href="#">Apply</a></li>
               </ul>
             </div>
@@ -248,10 +150,12 @@ function ListJobs() {
               Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temporinc ididunt ut dolore magna aliqua.
             </p>
             <h5>Job Nature: Full time</h5>
-            <p className="address"><span className="lnr lnr-map" /> 56/8, Panthapath Dhanmondi Dhaka</p>
-            <p className="address"><span className="lnr lnr-database" /> 15k - 25k</p>
+            <p className="address"><span className="fa-solid fa-map" /> 56/8, Panthapath Dhanmondi Dhaka</p>
+            <p className="address"><span className="fa-solid fa-database" /> 15k - 25k</p>
           </div>
-        </div>	
+        </div>
+
+
         <a className="text-uppercase loadmore-btn mx-auto d-block" href="category.html">Load More job Posts</a>
       </div>
       <div className="col-lg-4 sidebar">
@@ -267,118 +171,16 @@ function ListJobs() {
             <li><a className="justify-content-between d-flex" href="category.html"><p>Chicago</p><span>17</span></a></li>
           </ul>
         </div>
-        <div className="single-slidebar">
-          <h4>Top rated job posts</h4>
-          <div className="active-relatedjob-carusel">
-            <div className="single-rated">
-              <img className="img-fluid" src="img/r1.jpg" alt />
-              <a href="single.html"><h4>Creative Art Designer</h4></a>
-              <h6>Premium Labels Limited</h6>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temporinc ididunt ut dolore magna aliqua.
-              </p>
-              <h5>Job Nature: Full time</h5>
-              <p className="address"><span className="lnr lnr-map" /> 56/8, Panthapath Dhanmondi Dhaka</p>
-              <p className="address"><span className="lnr lnr-database" /> 15k - 25k</p>
-              <a href="#" className="btns text-uppercase">Apply job</a>
-            </div>
-            <div className="single-rated">
-              <img className="img-fluid" src="img/r1.jpg" alt />
-              <a href="single.html"><h4>Creative Art Designer</h4></a>
-              <h6>Premium Labels Limited</h6>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temporinc ididunt ut dolore magna aliqua.
-              </p>
-              <h5>Job Nature: Full time</h5>
-              <p className="address"><span className="lnr lnr-map" /> 56/8, Panthapath Dhanmondi Dhaka</p>
-              <p className="address"><span className="lnr lnr-database" /> 15k - 25k</p>
-              <a href="#" className="btns text-uppercase">Apply job</a>
-            </div>
-            <div className="single-rated">
-              <img className="img-fluid" src="img/r1.jpg" alt />
-              <a href="single.html"><h4>Creative Art Designer</h4></a>
-              <h6>Premium Labels Limited</h6>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temporinc ididunt ut dolore magna aliqua.
-              </p>
-              <h5>Job Nature: Full time</h5>
-              <p className="address"><span className="lnr lnr-map" /> 56/8, Panthapath Dhanmondi Dhaka</p>
-              <p className="address"><span className="lnr lnr-database" /> 15k - 25k</p>
-              <a href="#" className="btns text-uppercase">Apply job</a>
-            </div>																		
-          </div>
-        </div>							
-        <div className="single-slidebar">
-          <h4>Jobs by Category</h4>
-          <ul className="cat-list">
-            <li><a className="justify-content-between d-flex" href="category.html"><p>Technology</p><span>37</span></a></li>
-            <li><a className="justify-content-between d-flex" href="category.html"><p>Media &amp; News</p><span>57</span></a></li>
-            <li><a className="justify-content-between d-flex" href="category.html"><p>Goverment</p><span>33</span></a></li>
-            <li><a className="justify-content-between d-flex" href="category.html"><p>Medical</p><span>36</span></a></li>
-            <li><a className="justify-content-between d-flex" href="category.html"><p>Restaurants</p><span>47</span></a></li>
-            <li><a className="justify-content-between d-flex" href="category.html"><p>Developer</p><span>27</span></a></li>
-            <li><a className="justify-content-between d-flex" href="category.html"><p>Accounting</p><span>17</span></a></li>
-          </ul>
-        </div>
-        <div className="single-slidebar">
-          <h4>Carrer Advice Blog</h4>
-          <div className="blog-list">
-            <div className="single-blog " style={{background: '#000 url(img/blog1.jpg)'}}>
-              <a href="single.html"><h4>Home Audio Recording <br />
-                  For Everyone</h4></a>
-              <div className="meta justify-content-between d-flex">
-                <p>
-                  02 Hours ago
-                </p>
-                <p>
-                  <span className="lnr lnr-heart" />
-                  06
-                  <span className="lnr lnr-bubble" />
-                  02
-                </p>
-              </div>
-            </div>
-            <div className="single-blog " style={{background: '#000 url(img/blog2.jpg)'}}>
-              <a href="single.html"><h4>Home Audio Recording <br />
-                  For Everyone</h4></a>
-              <div className="meta justify-content-between d-flex">
-                <p>
-                  02 Hours ago
-                </p>
-                <p>
-                  <span className="lnr lnr-heart" />
-                  06
-                  <span className="lnr lnr-bubble" />
-                  02
-                </p>
-              </div>
-            </div>
-            <div className="single-blog " style={{background: '#000 url(img/blog1.jpg)'}}>
-              <a href="single.html"><h4>Home Audio Recording <br />
-                  For Everyone</h4></a>
-              <div className="meta justify-content-between d-flex">
-                <p>
-                  02 Hours ago
-                </p>
-                <p>
-                  <span className="lnr lnr-heart" />
-                  06
-                  <span className="lnr lnr-bubble" />
-                  02
-                </p>
-              </div>
-            </div>																		
-          </div>
-        </div>							
+       
+      							
       </div>
     </div>
   </div>	
 </section>
 {/* End post Area */}
 
-    
     </>
-  );
+  )
 }
 
-export default ListJobs;
+export default ListJobs
